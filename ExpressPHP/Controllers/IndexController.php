d7ae15d5e128b5bbf2d4852459e4218f6c758edb
Mayron <mayronfernandes01@gmail.com>
2022-01-11 20:12:38 -0300

    add default route

--- ExpressPHP/Controllers/IndexController.php
+++ ExpressPHP/Controllers/IndexController.php
@@ -4,6 +4,6 @@ namespace ExpressPHP\Controllers;
 
 class IndexController{
     public function index(){
-        echo "Eu sou a pagina padrão!!! <br> Você deve configurar o namespace dos seus controladores.";
+        echo "Olá mundo !!!";
     }
 }
--- ExpressPHP/Express/Express.php
+++ ExpressPHP/Express/Express.php
@@ -5,9 +5,12 @@
 class Express{
     // array para armazenar todas as rotas da aplicação
     private array $routes = array();
+    // a rota padrao que sera carregada quando uma rota desconhecida for chamada
+    private Route $defaultRoute;
     // o caminho onde o controlador de todos o caminhos esta
-    private string $controllers_path;
-
+    
+    public function __construct( private string $controllers_path ){}
+    
     // define o caminho onde os controladores estão
     public function setControllersPath(string $path){
         $this->controllers_path = $path;
@@ -19,46 +22,47 @@ class Express{
     }
 
     // cria um objeto de rota
-    public function get(string $route, string $controller, string $action){
-        try{
-            // se nenhum dos parametros estiverem vazios
-            if(!empty($route) && !empty($controller) && !empty($action)){
-                // cria um objeto para receber os parametros
-                $route_obj = new Route();
+    public function addRoute(string $route, string $controller, string $action, bool $is_default_route = false){
+        // verifica se nenhum dos parametros estiverem vazios
+        if(!empty($route) && !empty($controller) && !empty($action)){
+            // cria um objeto para receber os parametros
+            $route_obj = new Route();
 
-                $route_obj->route = $route;
-                $route_obj->controller = $controller;
-                $route_obj->action = $action;
+            $route_obj->route = $route;
+            $route_obj->controller = $controller;
+            $route_obj->action = $action;
 
-                // adiciona este objeto no array de rotas
-                array_push($this->routes, $route_obj);
-            }else{
-                throw new \Exception("Um parametro de rota esta vazio");
+            // adiciona este objeto no array de rotas
+            array_push($this->routes, $route_obj);
+            // verifica se e a rota definida como default e se a rota default está vazia
+            if($is_default_route && empty($this->defaultRoute)){
+                $this->defaultRoute = new Route;
+                $this->defaultRoute->route = $route;
+                $this->defaultRoute->controller = $controller;
+                $this->defaultRoute->action = $action;
+
+            }else if($is_default_route && !empty($this->defaultRoute)){
+                throw new \Exception("Você está definindo mais de uma rota como padrão");
             }
-            
-        }catch (\Exception $e){
-            return "Erro na pre-criação da rota : " . $e->getMessage();
+        }else{
+            throw new \Exception("Um parametro de rota esta vazio");
         }
     }
    
     // inicia os serviços
     public function listen(){
-        try{
-            // pega a rota atual
-            $url = $this->getUrl();
-            // se nenhum caminho alternativo para os controllers foi criado ele procura um controller interno 
-            if(empty($this->controllers_path)){
-                $this->runDefault();
-            }
-            else{
-                $this->run(routes: $this->routes, url: $url, controllers_path: $this->controllers_path);
-            }
+        if(empty($this->defaultRoute)){
+            throw new \Exception("Nenhuma rota foi definida como padrão");
         }
-        catch (\Exception $e){
-            echo "Erro na conexão com a rota : " . $e->getMessage();
-            $this->runDefault();
+        if(empty($this->controllers_path)){
+            throw new \Exception("O caminho para os controladores está vazio");
         }
+        // pega a rota atual
+        $url = $this->getUrl();
+        // se nenhum caminho alternativo para os controllers foi criado ele procura um controller interno 
+        $this->run(routes: $this->routes, url: $url, controllers_path: $this->controllers_path);
     }
+
     private function run(array $routes, string $url, string $controllers_path){
         // verifica qual a rota que esta ativa
         foreach($routes as $key => $value){
@@ -73,15 +77,21 @@ class Express{
                     if(method_exists($controller, $action)){
                         $controller->$action();
                         die();
+                    }else{
+                        throw new \Exception("A action '{$action}' que esta sendo requisitada não existe, ou não foi encontrada");
                     }
+                }else{
+                    throw new \Exception("O controlador '{$class}' que esta sendo requisitado não existe, ou não foi encontrado");
                 }
             }
         }
-        throw new \Exception("a rota solicitada não existe");
+        $this->runDefault();
     }
+
+    // executa a rota defaut
     private function runDefault(){
-        $class = "ExpressPHP\\Controllers\\IndexController";
-        $action = "index";
+        $class = $this->controllers_path . ucfirst($this->defaultRoute->controller);
+        $action = $this->defaultRoute->action;
         $controller = new $class;
         $controller->$action();
     }
